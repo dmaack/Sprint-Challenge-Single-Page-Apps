1aaf4d24339baf92bb599872d7a4474872058f19
dominique maack <dominiquemaack@gmail.com>
2019-09-20 09:46:47 -0600

    Character Cards created and elements displaying

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { Card, CardBody, CardTitle, CardText, CardImg } from "reactstrap";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const CharacterCard = props => {
+  return (
+    <Card>
+      <CardTitle>Character Name: {props.character.name}</CardTitle>
+    <CardImg
+      src={props.character.image}
+      alt="Characters Image"
+    />
+    <CardBody>
+      <CardText>Status: {props.character.status}</CardText>
+      <CardText>Species: {props.character.species}</CardText>
+      <CardText>Gender: {props.character.gender}</CardText>
+    </CardBody>
+  </Card>
+  )
 }
+export default CharacterCard;

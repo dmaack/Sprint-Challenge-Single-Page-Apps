28ba1bd0e2ac0da6dbba7416cc3f7b069e5cb52c
dominique maack <dominiquemaack@gmail.com>
2019-09-20 09:32:14 -0600

    Data displaying to screen from Character List

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,34 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
+  const [characters, setCharacters] = useState( [] );
   // TODO: Add useState to track data from useEffect
 
   useEffect(() => {
+    const getCharacters = () => {
+      axios 
+    .get(`https://rickandmortyapi.com/api/character/`)
+    .then(response => {
+      setCharacters(response.data.results);
+      console.log(response);
+    })
+    .catch(error => {
+      console.log('Not data received', error);
+    })
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    }
+    getCharacters();
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map(character => (
+        <CharacterCard key={character.id} character={character}/>
+      ))}
     </section>
   );
 }
